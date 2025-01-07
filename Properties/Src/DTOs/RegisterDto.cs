61b8d81da4d593fbba14c9848ecb11ef8987d73b
Thetrolxs <moralesharnisch@gmail.com>
2025-01-07 18:11:55 -0300

    feat: add login and register DTO

---
+++ Properties/Src/DTOs/RegisterDto.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blog_dotnet_api.Properties.Src.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set;}
+
+        [Required]
+        [MinLength(6, ErrorMessage = "La contraseña debe tener un minimo de 6 caracteres.")]
+        [RegularExpression(@"^(?=.*\d).+$", ErrorMessage = "La contraseña debe tener al menos 1 número")]
+        public string Password { get; set;}
+    }
+}
