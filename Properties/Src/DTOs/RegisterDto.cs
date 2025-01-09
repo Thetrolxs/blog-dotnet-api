43fbd3c1f393e2c0257cbcb1d5ec2345e55d2d7d
Thetrolxs <moralesharnisch@gmail.com>
2025-01-09 08:23:21 -0300

    fix: registerDto and add confirmPassword

--- Properties/Src/DTOs/RegisterDto.cs
+++ Properties/Src/DTOs/RegisterDto.cs
@@ -10,11 +10,14 @@ namespace blog_dotnet_api.Properties.Src.DTOs
     {
         [Required]
         [EmailAddress]
-        public string Email { get; set;}
+        public string Email { get; set;} = string.Empty;
 
         [Required]
         [MinLength(6, ErrorMessage = "La contraseña debe tener un minimo de 6 caracteres.")]
         [RegularExpression(@"^(?=.*\d).+$", ErrorMessage = "La contraseña debe tener al menos 1 número")]
-        public string Password { get; set;}
+        public string Password { get; set;} = string.Empty;
+
+        [Compare("Password", ErrorMessage ="Las constraseñas no coinciden.")]
+        public string confirmPassword {get; set;} = string.Empty;
     }
 }
