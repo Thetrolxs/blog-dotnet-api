cde809dd7dce9af39fac4c7549c89076b3ede7dc
Thetrolxs <moralesharnisch@gmail.com>
2025-01-09 17:11:14 -0300

    fix: var empty in loginDto

--- Properties/Src/DTOs/LoginDto.cs
+++ Properties/Src/DTOs/LoginDto.cs
@@ -10,9 +10,9 @@ namespace blog_dotnet_api.Properties.Src.DTOs
     {
         [Required]
         [EmailAddress]
-        public string Email { get; set;}
+        public string Email { get; set;} = string.Empty;
 
         [Required]
-        public string Password { get; set;}
+        public string Password { get; set;} = string.Empty;
     }
 }
