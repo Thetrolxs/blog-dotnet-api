d7b43b302131deaa71f3d4ebb4a09285407ce8e8
Thetrolxs <moralesharnisch@gmail.com>
2025-01-09 18:17:02 -0300

    feat: add post model

---
+++ Properties/Src/Entities/Post.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blog_dotnet_api.Properties.Src.Entities
+{
+    public class Post
+    {
+        public int Id {get; set;}
+        public string Title {get; set;} = string.Empty;
+        public DateTime PublishedDate {get; set;}
+        public string ImageUrl {get; set;} = string.Empty;
+        public int UserId {get; set;}
+        public User? User {get; set;}
+
+    }
+}
