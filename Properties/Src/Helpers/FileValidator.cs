84ff2a4b172553d363ac26aeb67a48d1d94b9488
Thetrolxs <moralesharnisch@gmail.com>
2025-01-10 06:36:42 -0300

    feat: add FileValidator helper

---
+++ Properties/Src/Helpers/FileValidator.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blog_dotnet_api.Properties.Src.Helpers
+{
+    public class FileValidator : ValidationAttribute
+    {
+        private readonly string[] ValidFormats = { "image/jpeg", "image/png", ".jpg", ".png"};
+        private readonly long MaxSize = 5 * 1024 * 1024;
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is IFormFile file)
+            {
+                if(!ValidFormats.Contains(file.ContentType))
+                {
+                    throw new Exception("Formato de la imagen no válido. Solo JPG y PNG");
+                }
+
+                if(file.Length > MaxSize)
+                {
+                    throw new Exception("El tamaño de la imagen no puede superar los 5 MB");
+                }
+            }
+
+            return new ValidationResult("El archivo es obligatorio");
+        }
+    }
+}
