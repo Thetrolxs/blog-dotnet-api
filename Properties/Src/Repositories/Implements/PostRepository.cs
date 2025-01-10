4936720053846e958d8283c06e65e25392865f1c
Thetrolxs <moralesharnisch@gmail.com>
2025-01-10 17:41:24 -0300

    feat: add post repository

---
+++ Properties/Src/Repositories/Implements/PostRepository.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using blog_dotnet_api.Properties.Src.Data;
+using blog_dotnet_api.Properties.Src.Entities;
+using blog_dotnet_api.Properties.Src.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace blog_dotnet_api.Properties.Src.Repositories.Implements
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly DataContext _context;
+
+        public PostRepository(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+        public async Task AddPost(Post post)
+        {
+            await _context.Posts.AddAsync(post);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Post>> GetPostsAsync()
+        {
+            var posts = await _context.Posts.Include(u => u.User).ToListAsync();
+            return posts;
+        }
+    }
+}
---
+++ Properties/Src/Repositories/Interface/IPostRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using blog_dotnet_api.Properties.Src.Entities;
+
+namespace blog_dotnet_api.Properties.Src.Repositories.Interface
+{
+    public interface IPostRepository
+    {
+        Task<IEnumerable<Post>> GetPostsAsync();
+        Task AddPost(Post post);
+    }
+}
