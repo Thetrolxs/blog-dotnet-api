40debb6eca08cb09621fbca3ff1c8abadaaef341
Thetrolxs <moralesharnisch@gmail.com>
2025-01-10 19:15:21 -0300

    feat: add photo service

---
+++ Properties/Src/Servicios/Implements/PhotoService.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using blog_dotnet_api.Properties.Src.Helpers;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.Extensions.Options;
+using Sprache;
+
+namespace blog_dotnet_api.Properties.Src.Servicios.Implements
+{
+    public class PhotoService : IPhotoService
+    {
+        private readonly Cloudinary _cloudinary;
+        public PhotoService(IOptions<CloudinarySetting> config)
+        {
+            var account = new Account
+            (
+                config.Value.CloudName,
+                config.Value.ApiKey,
+                config.Value.ApiSecret
+            );
+
+            _cloudinary = new Cloudinary(account);
+        }
+        public async Task<ImageUploadResult> AddPhoto(IFormFile photo)
+        {
+            var uploadResult = new ImageUploadResult();
+            if(photo.Length > 0)
+            {
+                using var stream = photo.OpenReadStream();
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(photo.FileName,stream),
+                    Transformation = new Transformation().Quality(80)
+                };
+
+                uploadResult = await _cloudinary.UploadAsync(uploadParams);
+            }
+
+            return uploadResult;
+        }
+    }
+}
