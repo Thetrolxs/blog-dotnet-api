468082dd19cff48806a928a7fd8122de49de86a8
Thetrolxs <moralesharnisch@gmail.com>
2025-01-10 23:04:12 -0300

    feat: add tokenDto

---
+++ Properties/Src/DTOs/TokenDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blog_dotnet_api.Properties.Src.DTOs
+{
+    public class TokenDto
+    {
+        public string Token {get; set;} = string.Empty;
+    }
+}
